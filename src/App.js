c3c38e01dfac4b52fa17bcb3ec14aa3db6b65982
Aaron Smith <smitham50@gmail.com>
2021-05-12 16:12:17 -0400

    fix dependency bug with react-transition-group

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation, Redirect } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import NavBar from './components/NavBar';
 import InfoBar from './components/InfoBar';
@@ -24,6 +24,7 @@ function App() {
           >
             <Switch location={location}>
               <Route exact path='/'>
+                <Redirect to='/about' />
               </Route>
               <Route path='/about'>
                 <InfoBar />
