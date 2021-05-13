0ceed918cac9bfc2ba411558ae932dae7594b4e0
Aaron Smith <smitham50@gmail.com>
2021-05-12 23:47:43 -0400

    add icon for email link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,10 +6,10 @@ const Contact = () => {
         <div className="contact">
             <img src={process.env.PUBLIC_URL + "Profile.jpg"} alt="" />
             <h2>Aaron Smith</h2>
-            <p>smitham50@gmail.com</p>
             <a href="https://www.linkedin.com/in/aaron-smith-830254192/" className="contact-link"><img className="contact-icon" src={process.env.PUBLIC_URL + "icons8-linkedin.svg"} alt="" /></a>
             <a href="https://github.com/smitham50" className="contact-link"><img className="contact-icon" src={process.env.PUBLIC_URL + "icons8-github.svg"} alt="" /></a>
             <a href="https://smitham50.medium.com/" className="contact-link"><img className="contact-icon" src={process.env.PUBLIC_URL + "icons8-medium-monogram.svg"} alt="" /></a>
+            <a href="mailto: smitham50@gmail.com"><img className="contact-icon" src={process.env.PUBLIC_URL + "icons8-gmail.svg"} alt="" /></a>
         </div>
     );
 }
