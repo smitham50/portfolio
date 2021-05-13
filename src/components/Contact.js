f122d9432489932b3d6ff2b7d4595d6f43b74f52
Aaron Smith <smitham50@gmail.com>
2021-05-12 22:32:32 -0400

    update package.json for deploy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,6 @@ const Contact = () => {
             <img src="Profile.jpg" alt="" />
             <h2>Aaron Smith</h2>
             <p>smitham50@gmail.com</p>
-            <p>917-671-7718</p>
             <a href="https://www.linkedin.com/in/aaron-smith-830254192/" className="contact-link"><img className="contact-icon" src="icons8-linkedin.svg" alt="" /></a>
             <a href="https://github.com/smitham50" className="contact-link"><img className="contact-icon" src="icons8-github.svg" alt="" /></a>
             <a href="https://smitham50.medium.com/" className="contact-link"><img className="contact-icon" src="icons8-medium-monogram.svg" alt="" /></a>
